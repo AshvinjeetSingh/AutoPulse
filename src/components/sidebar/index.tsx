bce01d379824311cbfb21ab24f38cd19931fdc2e
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-05-02 23:11:03 -0400

    fixed layout structures

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -15,42 +15,47 @@ import clsx from 'clsx'
 type Props = {}
 
 const SideMenu = (props: Props) => {
-    const pathName =usePathname()
-  return (
-      <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
-          <div className="flex items-center justify-center flex-col gap-8">
-            <link className='flex font-bold flex-row' href="/">AP</link>
-              <TooltipProvider>
-                {menuOptions.map((item)=>{
-                    <ul key={item.name}>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <li>
-                                    <Link
-                                        href={menuOptions.href}
-                                        className={clsx(
-                                            'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
-                                            {
-                                                'dark:bg-[#2F006B] bg-[#EEE0FF] ':
-                                                    pathName === menuOptions.href,
-                                            }
-                                        )}
-                                    >
-                                        <menuOptions.Component
-                                            selected={pathName === menuOptions.href}
-                                        />
-                                    </Link>
-                                </li>
-                            </TooltipTrigger>
-                            
-                        </Tooltip>
-                    </ul>
-                })}
-              </TooltipProvider>
-          </div>
-    </nav>
-    
-  )
+    const pathName = usePathname()
+    return (
+        <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+            <div className="flex items-center justify-center flex-col gap-8">
+                <TooltipProvider>
+                    {menuOptions.map((menuItem) => (
+                        <ul key={menuItem.name}>
+                            <Tooltip delayDuration={0}>
+                                <TooltipTrigger>
+                                    <li>
+                                        <Link
+                                            href={menuItem.href}
+                                            className={clsx(
+                                                'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
+                                                {
+                                                    'dark:bg-[#2F006B] bg-[#EEE0FF] ':
+                                                        pathName === menuItem.href,
+                                                }
+                                            )}
+                                        >
+                                            <menuItem.Component
+                                                selected={pathName === menuItem.href}
+                                            />
+                                        </Link>
+                                    </li>
+                                </TooltipTrigger>
+                                <TooltipContent
+                                    side="right"
+                                    className="bg-black/10 backdrop-blur-xl"
+                                >
+                                    <p>{menuItem.name}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </ul>
+                    ))}
+                </TooltipProvider>
+
+            </div>
+        </nav>
+
+    )
 }
 
 export default SideMenu
