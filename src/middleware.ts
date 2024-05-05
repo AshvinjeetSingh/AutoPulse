b4279956e74e5f42611c302ea9baa6a92bc254ee
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-05-05 12:47:13 -0400

    downgraded clerk to 4.29.9

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware({
-publicRoutes: [
-    '/',
-    '/api/clerk-webhook',
-    '/api/drive-activity/notification',
-    '/api/payment/success',
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/clerk-webhook",
+    "/api/drive-activity/notification",
+    "/api/payment/success",
   ],
   ignoredRoutes: [
-    '/api/auth/callback/discord',
-    '/api/auth/callback/notion',
-    '/api/auth/callback/slack',
-    '/api/flow',
-    '/api/cron/wait',
-  ],});
+    "/api/auth/callback/discord",
+    "/api/auth/callback/notion",
+    "/api/auth/callback/slack",
+    "/api/flow",
+    "/api/cron/wait",
+  ],
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
